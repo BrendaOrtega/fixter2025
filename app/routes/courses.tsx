64630bf2f73c1ab6bf7d181f64de4ce2992a6aa3
Blissito <fixtergeek@gmail.com>
2025-01-03 17:40:40 -0600

    courses replaced in routes

--- app/routes/courses.tsx
+++ app/routes/courses.tsx
@@ -1,3 +1,7 @@
-import { redirect } from "react-router";
+import { redirect, type LoaderFunctionArgs } from "react-router";
 
-export const loader = () => redirect("/cursos");
+export const loader = ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  url.pathname = url.pathname.replace("courses", "cursos");
+  return redirect(url.toString());
+};
