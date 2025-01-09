abbaaa8e52b8598f90e0a5f8d71214641293c6a0
BrendaOrtega <bremin11.20.93@gmail.com>
2025-01-09 12:03:37 -0600

    no prerender

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,18 +3,18 @@ import { db } from "./app/.server/db";
 
 export default {
   ssr: true,
-  async prerender() {
-    const posts = await db.post.findMany({
-      where: { published: true },
-      select: { slug: true },
-    });
-    const courses = await db.course.findMany({
-      where: { published: true },
-      select: { slug: true },
-    });
-    return ["/"]
-      .concat(posts.map((post) => `/blog/${post.slug}`))
-      .concat(courses.map((course) => `/cursos/${course.slug}/detalle`));
-    // return ["/", "/blog", "/cursos"];
-  },
+  // async prerender() {
+  //   const posts = await db.post.findMany({
+  //     where: { published: true },
+  //     select: { slug: true },
+  //   });
+  //   const courses = await db.course.findMany({
+  //     where: { published: true },
+  //     select: { slug: true },
+  //   });
+  //   return ["/"]
+  //     .concat(posts.map((post) => `/blog/${post.slug}`))
+  //     .concat(courses.map((course) => `/cursos/${course.slug}/detalle`));
+  //   // return ["/", "/blog", "/cursos"];
+  // },
 } satisfies Config;
